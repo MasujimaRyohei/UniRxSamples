b9d67d04922242db4635e5da5a457f5bdc767e13
mr <info@masujimaryohei.jp>
2019-03-05 14:54:00 +0900

    [Add] For Jenkins auto building.

---
+++ Assets/Editor/MyBuilder.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class MyBuilder
+{
+    // ビルド実行でAndroidのapkを作成する例
+    [UnityEditor.MenuItem("Tools/Build Project AllScene Android")]
+    public static void BuildProjectAllSceneAndroid()
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
+        List<string> allScene = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (scene.enabled)
+            {
+                allScene.Add(scene.path);
+            }
+        }
+        PlayerSettings.applicationIdentifier = "jp.masujimaryohei.unirxsamples";
+        PlayerSettings.statusBarHidden = true;
+        BuildPipeline.BuildPlayer(
+            allScene.ToArray(),
+            "unirxsamples.apk",
+            BuildTarget.Android,
+            BuildOptions.None
+        );
+    }
+}
