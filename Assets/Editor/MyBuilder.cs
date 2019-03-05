a3eb7d38cc484dff9c9e811ea1aa61da4a7ec4de
mr <info@masujimaryohei.jp>
2019-03-05 17:26:49 +0900

    [Update] MyBuilder script and AppIdentifier.

--- Assets/Editor/MyBuilder.cs
+++ Assets/Editor/MyBuilder.cs
@@ -2,15 +2,102 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.Build.Reporting;
 
 public class MyBuilder
 {
-    // ビルド実行でAndroidのapkを作成する例
+    /// <summary>
+    /// 識別名
+    /// </summary>
+    private const string BUNDLE_IDENTIFIER = "jp.masujimaryohei.unirxsamples";
+
+    /// <summary>
+    /// XcodeのDeviceプロジェクトが生成されるディレクトリ
+    /// </summary>
+    private const string IOS_DEVICE_LOCATION_PATH_NAME = "Xcode_Device";
+
+    /// <summary>
+    /// XcodeのSimulatorプロジェクトが生成されるディレクトリ
+    /// </summary>
+    private const string IOS_SIMULATOR_LOCATION_PATH_NAME = "Xcode_Simulator";
+
+    /// <summary>
+    /// 生成されるアプリケーション名
+    /// </summary>
+    private const string ANDROID_LOCATION_PATH_NAME = "Android.apk";
+
+    /// <summary>
+    /// Builds the project all scene android.
+    /// </summary>
     [UnityEditor.MenuItem("Tools/Build Project AllScene Android")]
     public static void BuildProjectAllSceneAndroid()
     {
-        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android,BuildTarget.Android);
+
+        PlayerSettings.Android.keystorePass = "keystorePass";//キーストアのパスワード
+        PlayerSettings.Android.keyaliasName = "keyaliasName";//エイリアス名
+        PlayerSettings.Android.keyaliasPass = "keyaliasPass";//エイリアスパスワード
+
+        CommonPlayerSettings();
+
+        BuildReport errorMessage = BuildPipeline.BuildPlayer(GetAllScene(), ANDROID_LOCATION_PATH_NAME, BuildTarget.Android, BuildOptions.None);
+        if (errorMessage==null)
+        {
+            Debug.Log("Android Build succeeded");
+        }
+        else
+        {
+            Debug.Log(errorMessage);
+        }
+    }
+
+    /// <summary>
+    /// Builds the project all scene iOS.
+    /// </summary>
+    [UnityEditor.MenuItem("Tools/Build Project AllScene iOS")]
+    public static void BuildProjectAllSceneiOS()
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.iOS,BuildTarget.iOS);
+
+        BuildOptions opt = BuildOptions.SymlinkLibraries |
+                           BuildOptions.AllowDebugging |
+                           BuildOptions.ConnectWithProfiler |
+                           BuildOptions.Development;
+
+        PlayerSettings.iOS.sdkVersion = iOSSdkVersion.DeviceSDK;
+
+        CommonPlayerSettings();
+
+        BuildReport errorMessage = BuildPipeline.BuildPlayer(GetAllScene(), IOS_DEVICE_LOCATION_PATH_NAME, BuildTarget.iOS, opt);
+        if (errorMessage == null)
+        {
+            Debug.Log("iOS Device Build succeeded");
+        }
+        else
+        {
+            Debug.Log(errorMessage);
+        }
+
+        /*
+        PlayerSettings.iOS.sdkVersion = iOSSdkVersion.SimulatorSDK;
+        errorMessage = BuildPipeline.BuildPlayer(GetAllScene(), IOS_SIMULATOR_LOCATION_PATH_NAME, BuildTarget.iOS, opt);
+        if(string.IsNullOrEmpty(errorMessage))
+        {
+            Debug.Log("iOS Simulator Build succeeded");
+        }else{
+            Debug.Log(errorMessage);
+        }
+        */
+    }
+
+    /// <summary>
+    /// Buildを行うシーンファイルを取り出す
+    /// </summary>
+    /// <returns>The all scene.</returns>
+    public static string[] GetAllScene()
+    {
         List<string> allScene = new List<string>();
+
         foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
         {
             if (scene.enabled)
@@ -18,13 +105,15 @@ public class MyBuilder
                 allScene.Add(scene.path);
             }
         }
-        PlayerSettings.applicationIdentifier = "jp.masujimaryohei.unirxsamples";
+        return allScene.ToArray();
+    }
+
+    /// <summary>
+    /// ios, Androidの共通の設定
+    /// </summary>
+    public static void CommonPlayerSettings()
+    {
+        PlayerSettings.applicationIdentifier = BUNDLE_IDENTIFIER;
         PlayerSettings.statusBarHidden = true;
-        BuildPipeline.BuildPlayer(
-            allScene.ToArray(),
-            "unirxsamples.apk",
-            BuildTarget.Android,
-            BuildOptions.None
-        );
     }
 }
